f6fe60983787c91a3d2a33d0bcead72a31c3261b
Kevin Leutzinger <me@kevinleutzinger.com>
2020-08-03 23:36:44 -0700

    also

--- timestamp.js
+++ timestamp.js
@@ -1,6 +1,15 @@
 // provides functions to write the current frame number and info to a file.
 // get config
 const { appendFile } = require('fs');
+const _ = require('lodash');
+var exec = require('child_process').exec;
+
+const config = {
+  dolphin_path :
+    'C:\\Users\\kevin\\AppData\\Roaming\\Slippi Desktop App\\dolphin.exe',
+  replay_path  :
+    '2360,C:\\Users\\kevin\\Documents\\Slippi\\Game_20200803T161502.slp'
+};
 
 function writeTimestamp(game_info, output_path) {
   console.log(`writing ${JSON.stringify(game_info)} to ${output_path}`);
@@ -15,5 +24,13 @@ function processTxtStamps(path) {
   // convert the text files to clippi-valid replay json files?
   return;
 }
+
+function launchReplay(path, info_obj) {
+  // C:\Users\kevin\AppData\Roaming\Slippi Desktop App\dolphin
+  exec(config.dolphin_path, function callback(error, stdout, stderr) {
+    // result
+  });
+}
+// launchReplay();
 // https://github.com/project-slippi/slippi-wiki/blob/master/COMM_SPEC.md
 module.exports = { writeTimestamp };
